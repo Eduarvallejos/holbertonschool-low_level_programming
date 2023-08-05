436436e67738fe8882e721b0fffc1f5a8d410c3c
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-05 08:37:17 -0700

    C language exercises

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - punto de entrada del programa.
+ * return: Retorna 0 al finalizar.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+	return (0);
 }

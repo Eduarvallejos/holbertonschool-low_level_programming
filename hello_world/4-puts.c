444ee1979a10af6b132bda8c5c5d4cfb5208aead
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-04 21:14:18 -0700

    C language exercises

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,1 @@
-#include <stdio.h> int main() { puts("Programming is like building a multilingual puzzle"); return 0; }
+#include <-Werror>  puts("Programming is like building a multilingual puzzle"); return 0; 

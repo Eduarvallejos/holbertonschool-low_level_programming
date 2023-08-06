0ccf52a7b1dd1b7e2aec2f2f1a4b3a408bf714bf
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-06 11:12:15 -0700

    variables,if,else,while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ last_digit = n % 10;
 
 if (last_digit > 5)
 {
-printf("last digit of %d is %dis and is greater than 5\n", n, last_digit);
+printf("last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {

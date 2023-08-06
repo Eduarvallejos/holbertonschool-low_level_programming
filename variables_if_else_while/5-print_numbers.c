17e8e6b5cfcb77099c4aef880f2895fdd5516fa3
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-06 13:11:36 -0700

    variables,if,else,while

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Punto de entrada del programa.
+ * Return: Siempre devuelve 0 .
+ */
+
+int main(void)
+{
+int number = 0;
+
+while (number <= 9)
+{
+printf("%d", number);
+number++;
+}
+
+printf("\n");
+
+return (0);
+}

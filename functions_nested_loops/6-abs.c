6be0358569db61823760ee8e86fa8d6cbbe0684a
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-13 11:08:16 -0700

    learning functions and nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Return the absolute value of a number.
+ * @n: Receive a number.
+ *
+ * Return: The absolute value of the input number.
+ */
+int _abs(int n)
+{
+return (abs(n));
+}

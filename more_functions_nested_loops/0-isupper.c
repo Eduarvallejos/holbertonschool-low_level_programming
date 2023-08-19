de2d36f1122c983ec54734a1a3741ec47ff09cf1
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-18 19:30:49 -0700

    learning more nested loops

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
+/**
+* _isupper - check if the character is uppercase or lowercase.
+* Return: will return 0 or 1.
+*/
 
 int _isupper(int c)
 {
-return c >='A' && c <= 'Z';
+return (c >= 'A' && c <= 'Z');
 }

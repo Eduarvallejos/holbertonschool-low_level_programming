fcd3733ce668d97c9222d4aaf5a23e1fc6f21831
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-20 13:01:29 -0700

    Pointers, Arrays, and Strings exercises

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse a string.
+ * @s: string to be reversed.
+ */
+
+void rev_string(char *s)
+{
+ int length = strlen(s);
+ int ;
+    for (i = 0; i < length / 2; i++) {
+        char temp = s[i];
+        s[i] = s[length - i - 1];
+        s[length - i - 1] = temp;
+     
+}
+}

cd6e9f6601303acd0308d2532b06ffa8b194acfe
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-20 15:17:58 -0700

    Pointers, Arrays, and Strings exercises

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_rev - reverse and print the string.
  * @s: chain name.
  */
 void print_rev(char *s)
 {
-	int length = 0;
+    int length = 0;
 
-	while (s[length] != '\0')
-	{
-	length++;
-	}
+    while (s[length] != '\0')
+    {
+        length++;
+    }
 
-	int i;
-
-	for (i = length - 1; i >= 0; i--)
-	{
-	putchar(s[i]);
-	}
+    int i;  // Declaración movida al principio del bloque
+    for (i = length - 1; i >= 0; i--)
+    {
+        putchar(s[i]);
+    }
 }
+

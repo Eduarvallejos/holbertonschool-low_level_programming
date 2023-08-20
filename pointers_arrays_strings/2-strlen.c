5a0c48b8f64058b17c30200da7c0a6ac3aeef4c3
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-20 10:39:07 -0700

    Pointers, Arrays, and Strings exercises

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Print in stdout the len of string.
+ * @s: Value a Read len.
+ *
+ * Return: the len of string.
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (s[++len] != 0);
+return (len);
+}

9222a5f8595289dd70cc044a33e2185076b674e1
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-20 15:09:25 -0700

    Pointers, Arrays, and Strings exercises

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_rev - print the string backwards.
+ * print_rev - reverse and print the string.
  * @s: chain name.
  */
 void print_rev(char *s)
@@ -13,9 +13,9 @@ void print_rev(char *s)
 	length++;
 	}
 
-	int i = length - 1;
+	int i;
 
-	for (i; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 	putchar(s[i]);
 	}

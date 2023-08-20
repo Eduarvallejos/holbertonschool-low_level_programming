9864b308e2a22bfd8ddbc3a8a01f8408d43ce704
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-19 21:28:58 -0700

    learning more nested loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - Will return numbers from 0 to 9.
+ * _putchar: will print each number.
+ */
+void print_numbers(void)
+{
+int i;
+for ( i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+}

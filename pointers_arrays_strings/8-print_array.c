d6f50f3b375078c2559433950223372275d6158d
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-20 17:21:43 -0700

    Pointers, Arrays, and Strings exercises

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print n element of array.
+ * @a: array pointed.
+ * @n: elements to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}

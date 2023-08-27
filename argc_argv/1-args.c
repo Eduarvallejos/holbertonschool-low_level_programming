618edcc58c8f6906e0146af55f273cf538a1f982
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-27 10:40:11 -0700

    solving exercises with argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - This program print in stdout the number of arguments.
+ * @argc: Number of arguments.
+ * @argv: show us the arguments.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }

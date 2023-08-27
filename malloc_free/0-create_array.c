432ca962dd7ac94b042796bdd9d31ecedca8c45b
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-08-27 13:30:12 -0700

    exercises from 0 to 4

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_array - This function create un pointer.
+ * @size: Size for our pointer.
+ * @c: Character for initialize the pointer.
+ *
+ * Return: Success return a pointer, Error NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int counter;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	counter = 0;
+	while (counter < size)
+	{
+	ptr[counter] = c;
+	counter++
+	}
+	return (ptr);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+solving exercises with malloc and free

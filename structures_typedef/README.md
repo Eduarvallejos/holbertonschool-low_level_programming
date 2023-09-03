51b171b4f9415e0a6d92ad1693e7193b49f9d1fc
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-03 10:51:25 -0700

    solving exercises with Structures, typedef (0-5)

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+
+learning to use Structures, typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+/**
+ * struct dog - dog information.
+ * @name: dogs name.
+ * @age: dogs age.
+ * @owner: dogs owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
